fe0c2c3d886c1b6c2cb81e0906b5bf7fe5e29e5a
Andrew Strovers <ADStrovers@gmail.com>
2015-03-04 13:47:44 -0600

    First question finished.

---
+++ script.js
@@ -0,0 +1,3 @@
+var question_one = parseInt(prompt("What is the scientific name for water?\n1. H2O\n2. CO2\n3. H1N1\n4. Water\n\nEnter the number of your choice below."));
+
+console.log(question_one)
