5d261ffb3b9e6301c89c78149a111e64791bcfb1
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-04 13:57:41 -0600

    Added test for questions

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 var question_one = parseInt(prompt("What is the scientific name for water?\n1. H2O\n2. CO2\n3. H1N1\n4. Water\n\nEnter the number of your choice below."));
 
 console.log(question_one)
+
+if (question_one === 1){
+	alert("Correct!");
+	}else{
+		alert("Wrong!");
+	}
